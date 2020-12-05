802fcab488178438fad79754a4f97f831626bd16
Mikko Forsman <mikko.o.forsman@gmail.com>
2020-12-05 23:36:31 +0200

    Base Kotlin project and answer to Day 1

---
+++ src/main/kotlin/AdventOfCode/Day1.kt
@@ -0,0 +1,15 @@
+package AdventOfCode
+
+import java.io.File
+
+object Day1 {
+    fun calculate2020() {
+        val file = File("src/main/resources/day1.txt")
+        val lines = file.readLines().map { it.toInt() }
+        lines.forEach { it ->
+            lines.forEach { it2 ->
+                if(it + it2 == 2020) println(it * it2)
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,4 @@
+import AdventOfCode.Day1
+fun main(args: Array<String>) {
+    Day1.calculate2020()
+}
